7d03b1ca5018f3fa0c8a004adb47e42ffa87c909
COL <3056990779@qq.com>
2020-09-02 15:14:20 +0800

    第一次提交

---
+++ src/Login_modules.java
@@ -0,0 +1,24 @@
+import java.awt.Container;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+import java.sql.*;
+
+
+public class Login_modules extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	JPanel backGround;						// ±³¾°Ãæ°å								
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
